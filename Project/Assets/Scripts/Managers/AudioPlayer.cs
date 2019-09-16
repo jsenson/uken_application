d6355c1e5c90e5551918be94f086ad52fd1d559c
Jon Senson <jonsenson@gmail.com>
2019-09-16 13:08:01 -0400

    - Added audio

---
+++ Project/Assets/Scripts/Managers/AudioPlayer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class AudioPlayer : MonoBehaviour {
+    [SerializeField] private AudioSource _music = null;
+    [SerializeField] private AudioSource _matchEffect = null;
+
+    void Start() {
+        if(_music != null) _music.Play();
+    }
+
+    void OnEnable() {
+        SpriteTile.onTilesMatched += PlayMatchEffect;
+    }
+
+    void OnDisable() {
+        SpriteTile.onTilesMatched -= PlayMatchEffect;
+    }
+
+    void PlayMatchEffect(SpriteTile t1, SpriteTile t2) {
+        if(_matchEffect != null) _matchEffect.PlayOneShot(_matchEffect.clip);
+    }
+}
--- README.md
+++ README.md
@@ -5,3 +5,5 @@ The ToN namespace are utility scripts written by me and pulled in from other pro
 Coder's Crux font: https://www.dafont.com/coders-crux.font
 Background sprite: https://8bitweapon.com/8-bit-weapon-bits-with-byte-background-png/
 Tile sprites: https://www.spriters-resource.com/
+Music: https://www.fesliyanstudios.com/royalty-free-music/downloads-c/8-bit-music/6
+Match sound: https://themushroomkingdom.net/media/smw/wav
